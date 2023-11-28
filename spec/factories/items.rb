ac94afe7da3473db5a7357794a097299c5d6a434
Rob Young <rhyoung214@gmail.com>
2023-11-27 17:27:38 -0800

    Setting up basic testing
    Created test factories for accounts and items

---
+++ spec/factories/items.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :item do
+    association :user
+    access_token { "access-sandbox-de3ce8ef-33f8-452c-a685-8671031fc0f6" }
+    plaid_item_id { "Ed6bjNrDLJfGvZWwnkQlfxwoNz54B5C97ejBr" }
+    bank_name { "Chase" }
+    transactions_cursor { "" }
+    is_active { "" }
+  end
+end
